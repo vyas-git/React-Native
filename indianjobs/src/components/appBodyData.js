f2326f0713a69d5ffb9b6d225c2728d8dedb6ef5
vyas <vyas@vyass-MacBook-Air.local>
2017-04-25 01:15:44 +0530

    Monday Night

--- indianjobs/src/components/appBodyData.js
+++ indianjobs/src/components/appBodyData.js
@@ -5,45 +5,117 @@ import {Header,Left, Button, Icon, Title, Body, Right,Content,Container,Card,Car
 
 export default class AppBodyData extends Component  {
 
-    render() {
-      let titles=this.props.data.map((data,index)=>(
+
+constructor(){
+
+  super();
+  this._onScroll = this._onScroll.bind(this)
+
+  this.state={
+
+    jobResults:[],
+    contentOffsetY:0,
+
+  };
+
+
+}
+_onScroll(e){
+    var contentOffset = e.nativeEvent.contentOffset.y;
+    this.state.contentOffsetY < contentOffset ? this.getMoreJobs() : console.log("Scroll Up");
+    this.setState({contentOffsetY: contentOffset});
+  }
+
+getMoreJobs(){
+  let titles=this.props.data.map((data,index)=>(
   <Card key={index}>
-                          <CardItem>
-                              <Left>
-                              <Text style={{fontWeight:'bold',fontSize: 20,color:'#AA00FF'}}>#{index+1}</Text>
-                                  <Body>
-                                      <Text>{data.jobtitle}</Text>
-                                      <Text note><Text style={{fontWeight:'bold'}}>Company</Text> : {data.company}</Text>
-                                  </Body>
-                              </Left>
-                          </CardItem>
-                          <CardItem>
+                      <CardItem>
+                          <Left>
+                          <Text style={{fontWeight:'bold',fontSize: 20,color:'#AA00FF'}}>#{index+1}</Text>
                               <Body>
-                                  <Text>{data.snippet}</Text>
-                                  <Button transparent textStyle={{color: '#AA00FF'}}>
-                                  <Icon style={{color: '#AA00FF'}} name="time" />
-                                  <Left><Text style={{textAlign:'left'}}> {data.date}</Text></Left>
-                                  </Button>
-                                  <Button style={{backgroundColor:'#263238'}} full>
-                                  <Text style={{color:'#fff'}}   >View/Apply Now</Text>
-                                  </Button>
+                                  <Text>{data.jobtitle}</Text>
+                                  <Text note><Text style={{fontWeight:'bold'}}>Company</Text> : {data.company}</Text>
                               </Body>
-                          </CardItem>
-                     </Card>
+                          </Left>
+                      </CardItem>
+                      <CardItem>
+                          <Body>
+                              <Text>{data.snippet}</Text>
+                              <Button transparent textStyle={{color: '#AA00FF'}}>
+                              <Icon style={{color: '#AA00FF'}} name="time" />
+                              <Left><Text style={{textAlign:'left'}}> {data.date}</Text></Left>
+                              </Button>
+                              <Button style={{backgroundColor:'#263238'}} full>
+                              <Text style={{color:'#fff'}}   >View/Apply Now</Text>
+                              </Button>
+                          </Body>
+                      </CardItem>
+                 </Card>
 
-      ));
-      return(
 
-    <Container>
-     <Content>
-       {titles}
+  ));
+
+  this.setState({
+
+    //jobResults:this.state.jobResults,
+
+  });
+
+
+
+}
+componentDidMount(){
+let titles=this.props.data.map((data,index)=>(
+<Card key={index}>
+                    <CardItem>
+                        <Left>
+                        <Text style={{fontWeight:'bold',fontSize: 20,color:'#AA00FF'}}>#{index+1}</Text>
+                            <Body>
+                                <Text>{data.jobtitle}</Text>
+                                <Text note><Text style={{fontWeight:'bold'}}>Company</Text> : {data.company}</Text>
+                            </Body>
+                        </Left>
+                    </CardItem>
+                    <CardItem>
+                        <Body>
+                            <Text>{data.snippet}</Text>
+                            <Button transparent textStyle={{color: '#AA00FF'}}>
+                            <Icon style={{color: '#AA00FF'}} name="time" />
+                            <Left><Text style={{textAlign:'left'}}> {data.date}</Text></Left>
+                            </Button>
+                            <Button style={{backgroundColor:'#263238'}} full>
+                            <Text style={{color:'#fff'}}   >View/Apply Now</Text>
+                            </Button>
+                        </Body>
+                    </CardItem>
+               </Card>
+
+
+));
+
+this.setState({
+
+  jobResults:titles,
+
+});
+
+
+
+
+}
+
+
+    render() {
+ return(
+
+  <Container>
+     <Content onScroll={this._onScroll}>
+{this.state.jobResults}
      </Content>
-     </Container>
+ </Container>
 
     );
-
-
-    }
+}
 
 onnavigate(title,company,snippet,url){
 
